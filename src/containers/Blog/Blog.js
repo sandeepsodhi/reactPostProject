028b5f1a0f3d801bb8f141b3802fae39b826b5c0
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-02-22 23:38:50 -0500

    removed node modules

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,7 +7,20 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount(){
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({ posts: response.data});
+                //console.log(response);
+            })
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
